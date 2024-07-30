07e755dd17afd0c7fd97948118a1ffb8da717142
anishpujari <anishpujari10@gmail.com>
2024-07-30 10:48:55 +0530

    resolved-files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import './Stylesheets/Root.css'; 
-import Navbar from './Components/Navbar.js'; 
-import { BrowserRouter, Routes, Route} from "react-router-dom"; 
-import Homepage from './Pages/Homepage';
-import Footer from './Components/Footer.js';
-import Achievement from './Pages/Achievement.js';
-import Resources from './Pages/Resources.js';
-import Aboutss from "./Pages/Aboutpage.js"
+import "./Stylesheets/Root.css";
+import Navbar from "./Components/Navbar.js";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./Pages/Homepage";
+import Footer from "./Components/Footer.js";
+import Achievement from "./Pages/Achievement.js";
+import Resources from "./Pages/Resources.js";
+import Aboutss from "./Pages/Aboutpage.js";
+import Events from "./Pages/Events.js";
 
 // import Resources from './Pages/Resources';
-// import Footer from './Components/Footer'; 
+// import Footer from './Components/Footer';
 
 function App() {
   return (
-    <> 
+    <>
       <BrowserRouter>
-        <Navbar /> 
+        <Navbar />
         <Routes>
           <Route element={<Homepage />} path="/" />
           <Route element={<Resources />} path="/resources" />
           <Route element={<Achievement />} path="/achievements" />
           <Route element={<Aboutss />} path="/about" />
+          <Route element={<Events />} path="/Events" />
         </Routes>
         <Footer />
       </BrowserRouter>
