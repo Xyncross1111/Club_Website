800124d196414a2e1162335ff6fcf7b7905f5fbf
TejasSingh022 <tejassingh8493@gmail.com>
2024-08-14 09:37:30 +0530

    Footer done

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,54 +1,60 @@
-import React from "react";
-import Style from "../Stylesheets/Footer.module.css";
-import logo from "../Assets/tcblogo.jpg";
-import linkedin from "../Assets/linkedin.svg";
-import instagram from "../Assets/instagram.svg";
-import twitter from "../Assets/twitter.svg";
+import React from 'react';
+import Style from '../Stylesheets/Footer.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCopyright } from '@fortawesome/free-regular-svg-icons';
+import { faLinkedinIn, faInstagram} from '@fortawesome/free-brands-svg-icons';
+import { faXTwitter as faTwitter } from '@fortawesome/free-brands-svg-icons';
+import footerIcon from '../Assets/footer.svg';
 
-function Footer() {
+const Footer = () => {
   return (
-    <div className={Style.footer}>
-      <div className={Style.left}>
-        <img className={Style.logo_class} src={logo} alt='logo'></img>
-        <span className={Style.imgText}> TheCodeBreakers </span>
-      </div>
+    <div>
+      <header></header>
+      <footer>
+        <div id={Style.footer}>
+          <div id={Style.footerContent}>
+            <p id={Style.footerTitle}>CODEBREAKERS</p>
+            <p id={Style.footerTag}>Seasoned. Nimble. Remote.</p>
+            <p id={Style.footerInfo}>Text about club</p>
+            <hr id={Style.footerLine} />
 
-      <div className={Style.right}>
-        <div className={Style.mainCont}>
-          <div className={Style.Information}>
-            <h2>Information</h2>
-            <ul className={Style.list}>
-              <li>FAQ</li>
-              <li>Blog</li>
-              <li>Support</li>
-            </ul>
-          </div>
-          <div className={Style.Club}>
-            <h2>Club</h2>
-            <ul className={Style.list2}>
-              <li>About Us</li>
-              <li>Careers</li>
-              <li>Contact Us</li>
-            </ul>
+            <img src={footerIcon} id={Style.footerImage} alt="footer-img"></img>
+
+            <div id={Style.footerRedirects}>
+              <div id={Style.footerCopyright}>
+                <FontAwesomeIcon icon={faCopyright} />
+                <p id={Style.footerCopyrightText}>Copyright Codebreakers</p>
+              </div>
+              <div id={Style.footerTerms}>
+                <ul>
+                  <li><a href="google.com">Terms</a></li>
+                  <li><a href="google.com">Privacy</a></li>
+                  <li><a href="google.com">Cookies</a></li>
+                </ul>
+              </div>
+              <div id={Style.footerSocial}>
+                <div id={Style.footerLinkedin}>
+                  <a href="google.com">
+                    <FontAwesomeIcon icon={faLinkedinIn} />
+                  </a>
+                </div>
+                <div id={Style.footerInstagram}>
+                  <a href="google.com">
+                    <FontAwesomeIcon icon={faInstagram} />
+                  </a>
+                </div>
+                <div id={Style.footerX}>
+                  <a href="google.com">
+                    <FontAwesomeIcon icon={faTwitter} />
+                  </a>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-        <div className={Style.Copyright}>
-          <h4>© Copyright 2022 - TheCodeBreakers</h4>
-          <ul>
-            <li>Terms</li>
-            <li>Privacy</li>
-            <li>Cookies</li>
-          </ul>
-        </div>
-      </div>
-
-      <div className={Style.rightmost}>
-        <img src={linkedin} alt="linkedin"></img>
-        <img src={twitter} alt="twitter"></img>
-        <img src={instagram} alt="instagram"></img>
-      </div>
+      </footer>
     </div>
   );
-}
+};
 
 export default Footer;
